ff43947390c179a65b1bf724a46bfca34bf3ae56
skibinalexander <skibinalexander@gmail.com>
2019-04-24 10:45:18 +0300

    Initial Commit

---
+++ CoreCollection/CoreCollection.h
@@ -0,0 +1,19 @@
+//
+//  CoreCollection.h
+//  CoreCollection
+//
+//  Created by Пользователь on 24/04/2019.
+//  Copyright © 2019 com.skibinalexander.ru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreCollection.
+FOUNDATION_EXPORT double CoreCollectionVersionNumber;
+
+//! Project version string for CoreCollection.
+FOUNDATION_EXPORT const unsigned char CoreCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreCollection/PublicHeader.h>
+
+
