76c092a483590deed086405ce7499b31db1eb8f7
skibinalexander <skibinalexander@gmail.com>
2019-04-24 11:24:31 +0300

    Создание шаблона проекта

---
+++ CoreCollection/Source/Abstract/Classes/CCDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  CCDelegate.swift
+//  CoreCollection
+//
+//  Created by Пользователь on 24/04/2019.
+//  Copyright © 2019 com.skibinalexander.ru. All rights reserved.
+//
+
+import Foundation
