83d1a9480f4e89a661b4ebdcc67bfa957bb8aaa3
skibinalexander <skibinalexander@gmail.com>
2019-04-24 12:43:23 +0300

    Удалены файлы проекта

--- CoreCollection/Source/Abstract/Section/CCViewSection.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CCViewSection.swift
-//  CoreCollection
-//
-//  Created by Пользователь on 24/04/2019.
-//  Copyright © 2019 com.skibinalexander.ru. All rights reserved.
-//
-
-import Foundation
