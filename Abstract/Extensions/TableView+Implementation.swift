e60c9dadef067ebd7d98e28a453a0f3ea12bc295
Skibin Alexander <skibinalexander@gmail.com>
2020-07-03 13:01:59 +0300

    Add extension for reuse cells

---
+++ Abstract/Extensions/TableView+Implementation.swift
@@ -0,0 +1,15 @@
+//
+//  TableView+Implementation.swift
+//  Parcel
+//
+//  Created by Skibin Alexander on 03.07.2020.
+//  Copyright © 2020 Skibin Development. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    func registerReuseble(nibName: String) {
+        self.register(UINib(nibName: nibName, bundle: nil), forCellReuseIdentifier: nibName)
+    }
+}
